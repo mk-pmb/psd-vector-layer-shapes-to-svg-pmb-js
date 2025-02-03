bd4becbe4a236a5b9335009182d79684382f18ea
mk-pmb <mk-pmb@users.noreply.github.com>
2025-02-03 05:16:56 +0100

    [test] Add a simple tree dumper.

---
+++ test/dumptree.js
@@ -0,0 +1,24 @@
+﻿'use strict';
+
+require('p-fatal');
+
+const libPsd = require('psd-pmb');
+
+async function cliMain() {
+  const psdFile = await libPsd.fromFile(process.argv[2]);
+  const tree = {
+    document: null,
+    ...psdFile.tree().export(),
+  };
+  let json = JSON.stringify(tree, null, 2);
+
+  json = json.replace(/\n( +)\{\n[\n -z]{0,128}\}/g, function f(orig, indent) {
+    const oneline = indent + orig.trim().replace(/\n\s*/g, ' ');
+    if (oneline.length >= 80) { return orig; }
+    return '\n' + oneline;
+  });
+  json = json.replace(/((?:^| {2})\{)\n\s*/g, '$1 ');
+  console.log(json);
+}
+
+cliMain();
